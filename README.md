fb6a5bf7d64a03a297a1ec016cbad5de96dc9ffb
123qwertyuiop123 <1443819827@qq.com>
2022-07-10 15:21:35 +0800

    后端和客户端

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# node-socket
+# 基于node和socket的简易通讯demo
+# 文件
+- index.js 为后端文件
+- index.html 为客户端文件
+# 启动
+- 前提需要安装node环境， 启动命令：nodemon index.js
+- 推荐在vs code中安装open live server插件，在html文件中鼠标右击选择open with live server启动客户端
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const http = require('http');
+const server = http.createServer(app);
+// const cors = require('cors')
+
+// 解决跨域问题
+// app.use(cors())
+
+const { Server } = require("socket.io");
+// 引入socket并监听端口
+const io = new Server(server, {
+    cors: true
+});
+io.listen(3002)
+
+// 使用
+io.on('connection', (socket) => {
+    socket.on('message', (msg) => {
+        io.emit('message', msg);
+    });
+    socket.on('image', (msg) => {
+        io.emit('image', msg);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
